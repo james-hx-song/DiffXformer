7b7b577676a20b3ce02e5853e29e38d634440ac6
Adit Kolli <aditk@gl1006.arc-ts.umich.edu>
2024-12-05 14:16:32 -0500

    Got log likelihood to work. Skeptical on whether generate_until works

--- models/transformerWrapper.py
+++ models/transformerWrapper.py
@@ -1,8 +1,12 @@
 from models.model import MultiHeadDiffAttention, TransModel
+import torch
 import torch.nn as nn
 from transformers import AutoTokenizer
 import torch.nn.functional as F
-import LM
+from lm_eval.api.model import LM
+from lm_eval.api.instance import Instance
+from lm_eval.api.registry import register_model
+
 
 @register_model("TransformerWrapper")
 class TransformerWrapper(LM):
@@ -17,39 +21,65 @@ class TransformerWrapper(LM):
         for request in requests:
             log_probability = 0
             input, expected_output = request.args
-            input = self.tokenizer(input, return_tensors="pt")
-            expected_output = self.tokenizer(expected_output, return_tensors="pt")
-            output_logits = nn.LogSoftmax(self.model.forward(input))
+            input = self.tokenizer(input, return_tensors="pt")['input_ids']
+            expected_output = self.tokenizer(expected_output, return_tensors="pt")['input_ids']
+            # print(expected_output.numpy())
+            log_softmax = nn.LogSoftmax(dim = 1)
+            model_input = torch.cat((input, expected_output), dim = 1) # self.tokenizer.convert_ids_to_tokens(
+            results = self.model.forward(model_input)
+
+            output_logits = log_softmax(results)
+            print(output_logits.shape)
             is_greedy = True
-            for expected_token in expected_output:
+            for index, expected_token in enumerate(expected_output[0][:]):
                 # Find probability that expected_token would be chosen from output_logits
-                token_id = self.tokenizer.convert_tokens_to_ids(expected_token)
-                log_probability += output_logits[token_id].item()
+                print(expected_token.numpy())
+                token_id = expected_token #self.tokenizer.convert_tokens_to_ids(expected_token)
+                # for x in range(output_logits.shape[2]):
+                #     log_probability += output_logits[0][index+input.shape[0]][x].item()
+                log_probability += output_logits[0][index+input.shape[0] - 1][token_id].item()
                 if is_greedy:
-                    for prob in output_logits:
-                        if prob.item() > output_logits[token_id].item():
+                    for prob in output_logits[0][index]:
+                        if prob.item() > output_logits[0][index][token_id].item():
                             is_greedy = False
-                input.append(expected_token)
-                output_logits = nn.LogSoftmax(self.model.forward(input))
+                # input = torch.cat(input, expected_token, dim = 0)
+                # output_logits = log_softmax(self.model.forward(input))
             toret.append((log_probability, is_greedy))
         return toret
 
 
     def loglikelihood_rolling(self, requests: list[Instance]) -> list[tuple[float, bool]]:
-        #...
         toret = []
         for request in requests:
             log_probability = 0
-            expected_output = request.args[0]
-            input = []
-            expected_output = self.tokenizer(expected_output, return_tensors="pt")
-            output_logits = nn.LogSoftmax(self.model.forward(input))
-            for expected_token in expected_output:
+            input, expected_output = request.args
+            input = self.tokenizer(input, return_tensors="pt")['input_ids']
+            expected_output = self.tokenizer(expected_output, return_tensors="pt")['input_ids']
+            # print(expected_output.numpy())
+            log_softmax = nn.LogSoftmax(dim = 1)
+            # results = self.model.forward(input)
+            results = self.model.forward(torch.cat((input, expected_output), dim = 1))
+
+            output_logits = log_softmax(results)
+            print(output_logits.shape)
+            is_greedy = True
+            for index, expected_token in enumerate(input[0][1:]):
+                print(expected_token.numpy())
+                token_id = expected_token #self.tokenizer.convert_tokens_to_ids(expected_token)
+                log_probability += output_logits[0][index-1][token_id].item()
+                if is_greedy:
+                    for prob in output_logits[0][index]:
+                        if prob.item() > output_logits[0][index - 1][token_id].item():
+                            is_greedy = False
+            for index, expected_token in enumerate(expected_output[0]):
                 # Find probability that expected_token would be chosen from output_logits
-                token_id = self.tokenizer.convert_tokens_to_ids(expected_token)
-                log_probability += output_logits[token_id].item()
-                input.append(expected_token)
-                output_logits = nn.LogSoftmax(self.model.forward(input))
+                print(expected_token.numpy())
+                token_id = expected_token #self.tokenizer.convert_tokens_to_ids(expected_token)
+                log_probability += output_logits[0][index+input.shape[0] - 1][token_id].item()
+                if is_greedy:
+                    for prob in output_logits[0][index]:
+                        if prob.item() > output_logits[0][index][token_id].item():
+                            is_greedy = False
             toret.append(log_probability)
         return toret
 
@@ -59,33 +89,39 @@ class TransformerWrapper(LM):
         for request in requests:
             input, gen_controls = request.args
             keep_generating = True
-            generated_tokens = []
+            generated_tokens = self.tokenizer(input, return_tensors="pt")['input_ids'].flatten().tolist()
             while keep_generating:
+                print(generated_tokens)
+                generated_token_text = self.tokenizer.convert_ids_to_tokens(generated_tokens)
+                print(generated_token_text)
                 # If we have exceeded max gen tokens
-                if len(generated_tokens) >= gen_controls["max_gen_toks"]:
+                if len(generated_tokens) >= gen_controls.get("max_gen_toks", 100):
+                    keep_generating = False
+                 # TODO: Last token is None sometimes, not sure if this is bug or because last token in generated_tokens is stop token
+                elif generated_token_text[-1] == None:
+                    generated_tokens.pop()
                     keep_generating = False
                 # If we have made any of the stop sequences
-                generated_text = self.tokenizer.convert_tokens_to_string(generated_tokens)
-                for stop_seq in gen_controls["until"]:
-                    if len(generated_text) > len(stop_seq) and generated_text[-len(stop_seq):] == stop_seq:
-                        keep_generating = False
-                        continue
+                else:
+                    generated_text = self.tokenizer.convert_tokens_to_string(generated_token_text)
+                    for stop_seq in gen_controls["until"]:
+                        if len(generated_text) > len(stop_seq) and generated_text[-len(stop_seq):] == stop_seq:
+                            print(stop_seq)
+                            print(generated_text[-len(stop_seq):])
+                            keep_generating = False
+                            continue
                 if not keep_generating:
                     continue
 
                 # Get logits based on what we generated so far and pick next token based off of those probabilities
-                output_logits = nn.LogSoftmax(self.model.forward(generated_tokens))
+                model_input = torch.tensor(generated_tokens).reshape(-1, 1)
+                log_softmax = nn.LogSoftmax(dim = -1)
+                output_logits = log_softmax(self.model.forward(model_input))
                 probs = F.softmax(output_logits, dim=-1)
-                next_token = self.transformer.convert_ids_to_tokens(torch.multinomial(probs, num_samples=1).item())
-                # next_token = ""
-                # highest_prob = float('-inf')
-                # for token_id in range(len(output_logits)):
-                #     if output_logits[token_id].item() >= highest_prob:
-                #         highest_prob = output_logits[token_id].item()
-                #         next_token = self.transformer.convert_ids_to_tokens(token_id)
-
+                next_token = torch.multinomial(probs[-1][0], num_samples=1).item()
                 # Append generated token to sequence
                 generated_tokens.append(next_token)
             # Append text to results
-            toret.append(self.tokenizer.convert_tokens_to_string(generated_tokens))
+            print("finished one request")
+            toret.append(self.tokenizer.convert_tokens_to_string(self.tokenizer.convert_ids_to_tokens(generated_tokens)))
         return toret
--- test_eval_wrapper.py
+++ test_eval_wrapper.py
@@ -2,22 +2,27 @@ from transformers import AutoTokenizer
 from models.model import TransModel
 from models.transformerWrapper import TransformerWrapper
 from config import StableLMConfig, ToyTransConfig
+from lm_eval.api.instance import Instance
 
+print("Starting")
+n_ctx=64
 config = ToyTransConfig(n_ctx=n_ctx)
 model = TransModel(config)
+print("Model ready")
 tokenizer_name = "HuggingFaceTB/SmolLM-135M"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 
 wrapper = TransformerWrapper(model, tokenizer_name)
 
-requests = [("context1", "continuation1"), ("context2", "continuation2")]
-res = wrapper.loglikelihood(requests)
-print(res)
+# requests = [("context1", "continuation1"), ("context2", "continuation2")]
+# requests = [Instance("loglikelihood", {}, ( "context1", "continuation1"), 0), Instance("loglikelihood", {},( "context1", "continuation1"), 1)]
+# res = wrapper.loglikelihood(requests)
+# print(res)
 
-requests = [("input1", {"until": "stop1"}), ("input2", {"until": "stop2"})]
-res = wrapper.greedy_until(requests)
+requests = [Instance("generate_until", {}, ("input1", {"until": ["stop1"], "max_gen_toks": 10}), 3), Instance("generate_until", {}, ("input2", {"until": ["stop2"], "max_gen_toks": 20}), 4)]
+res = wrapper.generate_until(requests)
 print(res)
 
 requests = ["input1", "input2"]
-res = lm.loglikelihood_rolling(requests)
+res = wrapper.loglikelihood_rolling(requests)
 print(res)
